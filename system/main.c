5f5009c1e28c332a2a943618794cbe53cc3dfea8
Junchao Yan <yjc801@gmail.com>
2014-02-10 16:29:17 -0500

    local changes

--- system/main.c
+++ system/main.c
@@ -5,8 +5,8 @@ void prch(void);
 int main(void){
 	int i;
 	kprintf("Hello World!\r\n");
-	resume(create(prch,1024,1,10,"proc A",0));
-	resume(create(prch,1024,1,5,"proc B",0));
+	resume(create(prch,1024,0,10,"proc A",0));
+	resume(create(prch,1024,0,5,"proc B",0));
 	for(i=0;i<4;i++){
 		kprintf("D\r\n");
 	}
