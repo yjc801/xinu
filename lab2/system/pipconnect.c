570e5b7f38110cc6f2267c5391c7f3c2e13c332d
Junchao Yan <yjc801@gmail.com>
2014-03-02 20:03:18 -0500

    local changes

--- lab2/system/pipconnect.c
+++ lab2/system/pipconnect.c
@@ -19,6 +19,7 @@ int	pipconnect(pipid32 pipid, pid32 writer, pid32 reader)
 		|| isbadpid(writer) 
 		|| isbadpid(reader)
 		|| (writer == reader)){
+		kprintf("Check1\n");
 		restore(mask);
 		return SYSERR;
 	}
@@ -31,6 +32,7 @@ int	pipconnect(pipid32 pipid, pid32 writer, pid32 reader)
 	if (piptr->pstate != PIPE_USED 
 		|| piptr->pwriter != INIT_PID
 		|| piptr->preader != INIT_PID) {
+		kprintf("Check2\n");
 		restore(mask);
 		return SYSERR;
 	}
@@ -39,6 +41,7 @@ int	pipconnect(pipid32 pipid, pid32 writer, pid32 reader)
 
 	if (prptr_writer->prpipside != -1
 		|| prptr_reader->prpipside != -1) {
+		kprintf("Check3\n");
 		restore(mask);
 		return SYSERR;
 	}
