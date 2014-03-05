fab9b2a25c64c74fd2ca9076b32c97889e8b48ff
Junchao Yan <yjc801@gmail.com>
2014-03-05 11:59:50 -0500

    local changes

---
+++ lab1/system/testTS.c
@@ -0,0 +1,55 @@
+#include <xinu.h>
+
+int prchcpu(char c, char d, int s);
+int prchio(char c, char d, int s);
+
+int prA,  prB,  prC, prD;
+
+int round;
+
+
+int main(int argc, char **argv){
+		round = 3000;
+		
+		kprintf("\n\nPS TEST:\n");
+		resume( create(prchcpu, 2000, TSSCHED, 20, "proc A", 3, 'A', 'a', 100) );
+		resume( create(prchcpu, 2000, TSSCHED, 20, "proc B", 3, 'B', 'b', 100) );
+		resume( create(prchio, 2000, TSSCHED, 20, "proc C", 3, 'C', 'c', 50) );
+		resume( create(prchio, 2000, TSSCHED, 20,  "proc D", 3, 'D', 'd', 100) );
+		
+		while (1) {
+			sleep(1);					
+		}
+		return OK;
+}
+
+int prchcpu(char c, char d, int s){
+//		sleepms(s);
+		kprintf("%c", d);
+		int i;
+		for(i=0; i<round; i++){
+			kprintf("%d",  proctab[currpid].prprio);
+			kprintf("%c",  c);
+		}
+		kprintf("%c", d);
+		return 0;
+}
+
+int prchio(char c, char d, int s){
+//		sleepms(s);
+		kprintf("%c", d);
+		int i, j;
+		int innerr = 3;
+		int outerr = round/innerr;
+		for(i=0; i<outerr; i++){
+			kprintf("%d", proctab[currpid].prprio);
+			for(j=0; j<innerr; j++)
+				kprintf("%c",  c);
+			sleepms(s);
+		}
+		kprintf("%c", d);
+		return 0;
+}
+
+Expected Output: the priorities of CPU processes shouldn't change (or slightly change because of kprintf).
+		the priorities of I/O processes should significantly increase and don't become negative numbers.
