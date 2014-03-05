5438f1a0558e2fb0e1a5d7badd6c558ced0cdcae
Junchao Yan <yjc801@gmail.com>
2014-03-05 01:04:24 -0500

    local changes

--- lab2/shell/xsh_search.c
+++ lab2/shell/xsh_search.c
@@ -34,6 +34,7 @@ shellcmd xsh_search(int32 pip) {
 		}
 		
 		mylen = pipread(pip, &buf[0],256);
+		printf("%d\n", mylen);
 		if (SYSERR == mylen) {
 		 	fprintf(stderr,"Unable to read from pipeline\n");
 			return SYSERR;
