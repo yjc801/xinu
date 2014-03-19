c329105d5a83f55f8c8fae0b994e5a07d6ce9a3c
Junchao Yan <yjc801@gmail.com>
2014-03-19 13:46:24 -0400

    local changes

--- lab3/system/create.c
+++ lab3/system/create.c
@@ -58,6 +58,8 @@ pid32	create(
     prptr->prmsg.start = 0;
     prptr->prmsg.count = 0;
 
+    for (i=0; i<prptr->prmsg.size; i++) prptr->prmsg.elems[i] = 0;
+
 	/* Set up initial device descriptors for the shell		*/
 
 	prptr->prdesc[0] = CONSOLE;	/* stdin  is CONSOLE device	*/
