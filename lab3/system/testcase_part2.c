27255a9a5b8a080c834b50520b44ab63d00d53a9
Junchao Yan <yjc801@gmail.com>
2014-03-21 14:55:09 -0400

    local changes

---
+++ lab3/system/testcase_part2.c
@@ -0,0 +1,41 @@
+
+uint32 recvbuf;
+
+int myrecvhandler(void) {
+	kprintf("msg received = %d\n", recvbuf);
+	return(OK);
+}
+ 
+void sender(pid32 receiver) {
+	sleep(3);
+	if( send(receiver, 20) == SYSERR ) {
+		kprintf("Fail to send msg 20!\r\n");
+	} else {
+		kprintf("Send msg 20 to receiver!\r\n");
+	}
+	return;
+}
+
+void receiver() {
+	if (registerrecv(&recvbuf, &myrecvhandler) != OK) {
+		kprintf("recv handler registration failed\n");
+		return;
+	}
+	while(1) {
+		sleep(1);
+	}
+	return;
+}
+ 
+int main(int argc, char **argv) {
+	pid32 spid, rpid;
+	rpid = create(receiver, 2014, 20, "receiver", NULL);
+	spid = create(sender, 2048, 20, "sender", 1, rpid);
+	resume(rpid);
+	resume(spid);
+
+	while(1) {
+		sleep(100);
+	}
+	return OK;
+}
