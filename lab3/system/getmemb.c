0f08a2a2a4b8144483897b92ce31abe413313789
Junchao Yan <yjc801@gmail.com>
2014-03-21 23:20:04 -0400

    local changes

---
+++ lab3/system/getmemb.c
@@ -0,0 +1,59 @@
+/* getmemb.c - getmemb */
+
+#include <xinu.h>
+
+/*------------------------------------------------------------------------
+ *  getmem  -  Allocate heap storage, returning lowest word address
+ *------------------------------------------------------------------------
+ */
+char  	*getmemb(
+	  uint32	nbytes		/* size of memory requested	*/
+	)
+{
+	intmask	mask;			/* saved interrupt mask		*/
+	struct	memblk	*prev, *curr, *leftover;
+	struct  procent	*prptr;
+
+	mask = disable();
+	if (nbytes == 0) {
+		restore(mask);
+		return (char *)SYSERR;
+	}
+
+	nbytes = (uint32) roundmb(nbytes);	/* use memblk multiples	*/
+	
+	prptr = &proctab[currpid];
+
+	prev = &memlist;
+	curr = memlist.mnext;
+	while (curr != NULL) {			/* search free list	*/
+
+		if (curr->mlength == nbytes) {	/* block is exact match	*/
+			prev->mnext = curr->mnext;
+			memlist.mlength -= nbytes;
+			curr->gcpid = currpid;
+			curr->gcflag = FALSE;
+			prptr->prgcflag = TRUE;
+			restore(mask);
+			return (char *)(curr);
+
+		} else if (curr->mlength > nbytes) { /* split big block	*/
+			leftover = (struct memblk *)((uint32) curr +
+					nbytes);
+			prev->mnext = leftover;
+			leftover->mnext = curr->mnext;
+			leftover->mlength = curr->mlength - nbytes;
+			memlist.mlength -= nbytes;
+			curr->gcpid = currpid;
+			curr->gcflag = FALSE;
+			prptr->prgcflag = TRUE;
+			restore(mask);
+			return (char *)(curr);
+		} else {			/* move to next block	*/
+			prev = curr;
+			curr = curr->mnext;
+		}
+	}
+	restore(mask);
+	return (char *)SYSERR;
+}
