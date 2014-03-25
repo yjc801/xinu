e67d9980ee148cc7cfca4994da187c2e8df49674
Junchao Yan <yan114@xinu12.cs.purdue.edu>
2014-03-25 01:16:11 -0400

    remote changes

--- lab3/system/main.c
+++ lab3/system/main.c
@@ -8,16 +8,23 @@
 /************************************************************************/
 
 void allocater(void){
+	char *p1;
+	char *p2;
+	char *p3;
+
 	kprintf("pid %d\r\n",currpid);	
-	freememb(getmemb(10),10);
+	p1 = getmemb(10);
 	kprintf("1. %d\r\n",numbytes);
-//	sleepms(10);
-//	getmemb(8);
-//	kprintf("2. %d\r\n",numbytes);
-//	sleepms(10);
-//	getmemb(13);
-//	kprintf("3. %d\r\n",numbytes);
-	//sleepms(10);
+	sleepms(10);
+	p2 = getmemb(8);
+	kprintf("2. %d\r\n",numbytes);
+	sleepms(10);
+	p3 = getmemb(13);
+	kprintf("3. %d\r\n",numbytes);
+	freememb(p1,10);
+	freememb(p2,8);
+	freememb(p3,13);
+	sleepms(10);
 }
 
 int main(int argc, char **argv) {
