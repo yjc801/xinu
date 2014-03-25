fdfcba6a96eb951462cea936e02d9968081748f7
Junchao Yan <yjc801@gmail.com>
2014-03-24 21:43:54 -0400

    local changes

--- lab3/system/main.c
+++ lab3/system/main.c
@@ -9,18 +9,18 @@
 
 void allocater(void){
 	getmemb(10);
-	kprintf("1. # of bytes %d\r\n",numbytes);
+	kprintf("1. %d\r\n",numbytes);
 	getmemb(8);
-	kprintf("2. # of bytes %d\r\n",numbytes);
+	kprintf("2. %d\r\n",numbytes);
 	getmemb(13);
-	kprintf("3. # of bytes %d\r\n",numbytes);
+	kprintf("3. %d\r\n",numbytes);
 }
 
 int main(int argc, char **argv) {
 	pid32 pid;
-	kprintf("Start: # of bytes %d\r\n",numbytes);
+	kprintf("Start: %d\r\n",numbytes);
 	pid = resume(create(allocater, 2014, 20, "allocater", NULL));
 	kill(pid);
-	kprintf("End: # of bytes %d\r\n",numbytes);
+	kprintf("End: %d\r\n",numbytes);
 	return OK;
 }
