4894dcc0dd3a1239ed5604d17997f9271dc1626b
Junchao Yan <yjc801@gmail.com>
2014-03-28 11:38:40 -0400

    local changes

--- lab2/system/pipread.c
+++ lab2/system/pipread.c
@@ -20,11 +20,11 @@ int32	pipread(pipid32 pipid, char *buf, uint32 len)
 	piptr = &piptab[pipid];
 
 	// check if the process is the reader
-	if (piptr->preader != currpid
-		|| piptr->pstate != PIPE_CONNECTED){
-		restore(mask);
-		return SYSERR;
-	}
+	// if (piptr->preader != currpid
+	// 	|| piptr->pstate != PIPE_CONNECTED){
+	// 	restore(mask);
+	// 	return SYSERR;
+	// }
 
 	// check if buffer is full, if yes, put on the semaphor
 	int32 count, temp;
