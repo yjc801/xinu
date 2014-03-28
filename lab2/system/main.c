ca4e3b42d53335d0af7e7568cb28de80c44d1124
Junchao Yan <yjc801@gmail.com>
2014-03-28 14:54:41 -0400

    local changes

--- lab2/system/main.c
+++ lab2/system/main.c
@@ -76,7 +76,7 @@ resume(rdpid);
 if (SYSERR == pipdelete(pip)) {
 kprintf("[main] delete fail\r\n");
 }
-
+kprintf("main: Again\r\n");
 sleep(2);
 pip = pipcreate();
 wrpid = create(writer, 2048, 20, "writer", 2, pip, 1000);
