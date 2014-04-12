54b742a4aeb41baea80b3c7f26bec226ba170f62
Junchao Yan <yjc801@gmail.com>
2014-04-12 00:11:55 -0400

    local changes

--- lab3/system/main.c
+++ lab3/system/main.c
@@ -1,47 +1,65 @@
 #include <xinu.h>
-#include <stdio.h>
-#include <string.h>
-/************************************************************************/
-/* */
-/* main - main program for testing Xinu */
-/* */
-/************************************************************************/
-void sender(pid32 receiver) {
-	uint32 i;
-	for(i=0; i<15; i++) {
-		if( sendb(receiver, i) == SYSERR ) {
-			kprintf("Fail to send msg %d!\r\n", i);
-		} else {
-			kprintf("Send msg %d to receiver!\r\n", i);
-		}
+
+
+void PrintList(void);
+void RequestMem(int base, int round, bool8 sflag, int pflag);
+int main (int argc, char **argv) {
+
+	kprintf("Heap: %x, size %d\r\n", memlist.mnext, memlist.mnext->mlength);
+//	char *addr = getmem(560);
+//	kprintf("return addr: %x\r\n", addr);
+	resume(create(RequestMem, 2048, 20, "Test1", 4, 300, 10, 0, 1));
+	sleep(5);
+	resume(create(RequestMem, 2048, 20, "Test2", 4, 300, 10, 0, 2));
+	sleep(5);
+
+	resume(create(RequestMem, 2048, 20, "Test3-1", 4, 400, 20, 1, 10));
+	resume(create(RequestMem, 2048, 20, "Test3-2", 4, 400, 20, 1, 5));
+	sleep(10);
+	kprintf("Final\r\n");
+	PrintList();
+	return 1;
+
+}
+
+
+void PrintList(void) {
+
+	struct memblk *root;
+
+	root = memlist.mnext;
+	kprintf("Block List:\r\n");
+	while (NULL != root) {
+		kprintf("addr %x, size %d\r\n", root, root->mlength);
+		root = root->mnext;
 	}
+
 	return;
 }
 
-void receiver(void) {
-	int i;
-	uint32 msg;
-	for(i=0; i<18; i++) {
-		msg = receiveb();
-		if( msg == SYSERR ) {
-			kprintf("Fail to recieve msg!\r\n");
-		} else {
-			kprintf("Recieve msg %d from sender!\r\n", msg);
-		}
-	sleep(1);
-	}
+void RequestMem(int base, int round, bool8 sflag, int pflag) {
+
+	int i, len, prevlen;
+	if (pflag)
+		sleep(2);
+	char *prev, *curr;
+	prev = getmemgb(7);
+	kprintf("new %d, %x, siz %d\r\n", 0, prev, 7);
+	for (i=1; i<round; ++i) {
+		len = 7 + base * i;
+		curr = getmemgb(len);
+		if (sflag)
+			sleepms(100);
+		
+		prevlen = 7 + base * (i-1);
+		if (0 == i % pflag)
+			freememgb(prev, prevlen);
+		prev = curr;
+	}	
+	if (pflag > 5)
+		sleepms(1500);
+	kprintf("\r\n");
+	PrintList();
 	return;
 }
- 
-int main(int argc, char **argv) {
-	pid32 spid, rpid;
-	rpid = create(receiver, 2014, 20, "receiver", NULL);
-	spid = create(sender, 2048, 20, "sender", 1, rpid);
-	resume(rpid);
-	resume(spid);
 
-	while(1) {
-		sleep(100);
-	}
-	return OK;
-}
